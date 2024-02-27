f4f0f48ad11b761a02031e60f195aa28f326c4a1
Jimmy <jimmylo1606@gmail.com>
2024-02-27 17:22:19 -0500

    Add scaffolding for modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { dbConfig } from 'db/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(dbConfig)],
   controllers: [AppController],
   providers: [AppService],
 })
