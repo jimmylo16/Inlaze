acea96cd30e62ae3170195a3be822be6a7d10056
Jimmy <jimmylo1606@gmail.com>
2024-02-27 17:13:06 -0500

    Add configuration files

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { TransformInterceptor } from './common/interceptors/TransformResponse.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  const logger = new Logger('Bootstrap');
+
+  app.setGlobalPrefix('api', { exclude: ['/'] });
+
+  const config = new DocumentBuilder()
+    .setTitle('Helix Medical Info')
+    .setDescription('The Medical Info api')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  // Use globaly
+  app.useGlobalInterceptors(new TransformInterceptor());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //Remove objects properties with no decortors
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen(process.env.PORT || 3001);
+  logger.log(`App running on port ${process.env.PORT}`);
 }
 bootstrap();
